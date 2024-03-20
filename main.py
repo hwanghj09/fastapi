214bc9fbfac5b68e09dfb3325c881fb132006eab
hwanghj09 <101453046+hwanghj09@users.noreply.github.com>
2024-03-21 00:45:43 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-from fastapi import FastAPI, HTTPException, Form, Request, Cookie
-from fastapi.responses import RedirectResponse, FileResponse
+from fastapi import FastAPI, HTTPException, Form, Request, Cookie,Response
+from fastapi.responses import FileResponse, JSONResponse
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 from database import engineconn
 from model import User
-import http.cookies
+from itsdangerous import URLSafeSerializer
 
 app = FastAPI()
 templates = Jinja2Templates(directory='./public')
 engine = engineconn()
 session = engine.sessionmaker()
-
+SECRET_KEY = "JEOFIGHTING"
+serializer = URLSafeSerializer(SECRET_KEY)
 
 @app.get("/")
 def main():
@@ -24,10 +25,6 @@ def main():
 def register(request: Request):
     return templates.TemplateResponse('register.html', context={'request': request})
 
-@app.get("/check_grade")
-def check_grade(request: Request):
-    return templates.TemplateResponse('check_grade.html', context={'request': request})
-
 @app.get("/login")
 def login(request: Request):
     return templates.TemplateResponse('login.html', context={'request': request})
@@ -40,17 +37,30 @@ def community(request: Request):
 def process_register(username: str = Form(...), password: str = Form(...)):
     existing_user = session.query(User).filter_by(name=username).first()
     if existing_user:
-        raise HTTPException(status_code=400, detail="Username already registered")
+        return FileResponse('public/register.html')
     new_user = User(name=username, password=password)
     session.add(new_user)
     session.commit()
+    
     response = FileResponse("public/index.html")  # 회원가입 성공 시 /index로 이동
+    
+    # 유저 정보를 쿠키에 저장 (예시로 만료 날짜는 30일 후로 설정)
+    encrypted_username = serializer.dumps(username)
+    response.set_cookie(key="username", value=encrypted_username, max_age=30*24*60*60)  # 30일의 초로 설정
+    
     return response
 
 @app.post("/post_login")
 def process_login(request: Request, username: str = Form(...), password: str = Form(...)):
     user = session.query(User).filter_by(name=username, password=password).first()
     if not user:
-        raise HTTPException(status_code=401, detail="Invalid username or password")
+        return FileResponse('public/index.html')  # 로그인 실패 시 로그인 페이지로 리다이렉트
+    
     response = FileResponse('public/index.html')  # 로그인 성공 시 /index.html 파일을 반환
+    
+    # 유저 정보를 쿠키에 저장 (예시로 만료 날짜는 30일 후로 설정)
+    encrypted_username = serializer.dumps(username)
+    response.set_cookie(key="username", value=encrypted_username, max_age=30*24*60*60)  # 30일의 초로 설정
+    
     return response
+        
