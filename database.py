d72bd9306265df06f5fdd0c1def330df073cb0d0
hwanghj09 <101453046+hwanghj09@users.noreply.github.com>
2024-05-18 20:17:04 +0900

    Update database.py

--- database.py
+++ database.py
@@ -1,12 +1,10 @@
 from sqlalchemy import *
 from sqlalchemy.orm import sessionmaker
 
-USERS_DB_URL = 'mysql+pymysql://root:qwaszx77^^@svc.sel4.cloudtype.app:31168/users'
-SHOPPING_DB_URL = 'mysql+pymysql://root:qwaszx77^^@svc.sel4.cloudtype.app:31168/shopping'
-
+DB_URL = 'postgresql+psycopg2://hwanghj09:TZy2fh4gbYku@ep-quiet-water-a2l0cgw1.eu-central-1.pg.koyeb.app/koyebdb'
 class Usersengineconn:
     def __init__(self):
-        self.engine = create_engine(USERS_DB_URL, pool_recycle = 500)
+        self.engine = create_engine(DB_URL, pool_recycle = 500)
         self.SessionLocal = sessionmaker(bind=self.engine)
 
     def sessionmaker(self):
@@ -20,7 +18,7 @@ class Usersengineconn:
         
 class Shoppingengineconn:
     def __init__(self):
-        self.engine = create_engine(SHOPPING_DB_URL, pool_recycle = 500)
+        self.engine = create_engine(DB_URL, pool_recycle = 500)
         self.SessionLocal = sessionmaker(bind=self.engine)
 
     def sessionmaker(self):
